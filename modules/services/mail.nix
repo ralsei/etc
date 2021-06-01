b91677dff469e7f18d18e21704177c2e80b39010
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-06-01 09:29:47 -0400

    some secrets

--- modules/services/mail.nix
+++ modules/services/mail.nix
@@ -1,26 +1,9 @@
 { config, pkgs, lib, ... }:
-<<<<<<< HEAD
-let
-  cfg = config.hazel.services.mailserver;
-  release = "nixos-20.09";
-in 
-with lib; {
-  # couldn't get `niv` to work here, but whatev
-  imports = [
-    (builtins.fetchTarball {
-      # wew
-      url = 
-        "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/${release}/nixos-mailserver-${release}.tar.gz";
-      sha256 = "0vsvgxxg5cgmzwj98171j7h5l028f1yq784alb3lxgbk8znfk51y";
-    })
-  ];
-=======
 {
   mailserver = {
     enable = true;
     fqdn = "mail.knightsofthelambdacalcul.us";
     domains = [ "knightsofthelambdacalcul.us" ];
->>>>>>> refs/remotes/origin/canon
 
     loginAccounts = {
       "hazel@knightsofthelambdacalcul.us" = {
